0d4cd8413d3e7bf30d754231c041353d78c07501
1PSE <1tjdms0414@naver.com>
2026-06-24 19:07:37 +0900

    WebConfig.java 수정

--- src/main/java/likelion/todo/global/config/WebConfig.java
+++ src/main/java/likelion/todo/global/config/WebConfig.java
@@ -1,14 +1,16 @@
 package likelion.todo.global.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:")
+                .allowedOrigins("http://localhost:*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .maxAge(3600);
